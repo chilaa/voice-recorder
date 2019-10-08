f0b9e5650ac59350036a15486085fc2749a03b77
Farna Chilashvili <farnnn.ff@gmail.com>
2019-10-08 19:05:28 +0400

    first commit

---
+++ custom.js
@@ -0,0 +1,7 @@
+// document.getElementById("start").addEventListener("click", startRecording());
+// function startRecording() {
+//     alert("hey")
+// }
+console.log(document.getElementById("start")).addEventListener('click', function (e) {
+    alert("hey")
+})
