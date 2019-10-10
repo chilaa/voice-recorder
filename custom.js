5498cb6452d9339b3be70f8649420879da3fcf25
Farna Chilashvili <farnnn.ff@gmail.com>
2019-10-10 23:46:21 +0400

    made changes

--- custom.js
+++ custom.js
@@ -1,7 +1,39 @@
-// document.getElementById("start").addEventListener("click", startRecording());
-// function startRecording() {
-//     alert("hey")
-// }
-console.log(document.getElementById("start")).addEventListener('click', function (e) {
-    alert("hey")
-})
+let start = document.getElementById("start");
+let resume = document.getElementById("resume");
+let stop = document.getElementById("stop");
+let upload = document.getElementById("upload");
+
+console.log(navigator);
+if (navigator.mediaDevices){
+    console.log("is");
+
+    let constraints = { audio: true};
+    let chunks = [];
+
+    navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+        let mediaRecorder = new MediaRecorder(stream);
+        visualize(stream);
+
+        record.onClick = function() {
+            mediaRecorder.start();
+            console.log(mediaRecorder.state);
+        }
+    })
+}
+
+start.addEventListener("click",startRecording);
+resume.addEventListener('click', resumeRecording);
+stop.addEventListener('click', stopRecording);
+upload.addEventListener('click', uploadRecord);
+
+function startRecording() {
+}
+
+function uploadRecord() {
+}
+
+function stopRecording() {
+
+}
+function resumeRecording() {
+}
